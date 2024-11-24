fc762e611dfa0a1802844bb5f7eb521b8f7bfae5
Miftakhu_Rrizqi <115737352+rizqi0919@users.noreply.github.com>
2024-11-24 22:34:36 +0700

    Add files via upload

---
+++ dist/js/script.js
@@ -0,0 +1,73 @@
+// Navbar Fixed
+window.onscroll = function () {
+  const header = document.querySelector('header');
+  const fixedNav = header.offsetTop;
+  const toTop = document.querySelector('#to-top');
+
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add('navbar-fixed');
+    toTop.classList.remove('hidden');
+    toTop.classList.add('flex');
+  } else {
+    header.classList.remove('navbar-fixed');
+    toTop.classList.remove('flex');
+    toTop.classList.add('hidden');
+  }
+};
+
+// Hamburger
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click', function () {
+  hamburger.classList.toggle('hamburger-active');
+  navMenu.classList.toggle('hidden');
+});
+
+// Klik di luar hamburger
+window.addEventListener('click', function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove('hamburger-active');
+    navMenu.classList.add('hidden');
+  }
+});
+
+// Darkmode toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function () {
+  if (darkToggle.checked) {
+    html.classList.add('dark');
+    localStorage.theme = 'dark';
+  } else {
+    html.classList.remove('dark');
+    localStorage.theme = 'light';
+  }
+});
+
+// pindahkan posisi toggle sesuai mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  var readMoreBtn = document.getElementById('read-more');
+  var text = document.getElementById('text');
+  var originalText = text.innerHTML;
+  var truncatedText = originalText.substring(0, 100) + '...';
+
+  text.innerHTML = truncatedText;
+
+  readMoreBtn.addEventListener('click', function() {
+      if (text.innerHTML === truncatedText) {
+          text.innerHTML = originalText;
+          readMoreBtn.textContent = 'Lebih Sedikit';
+      } else {
+          text.innerHTML = truncatedText;
+          readMoreBtn.textContent = 'Lebih Banyak....';
+      }
+  });
+});
